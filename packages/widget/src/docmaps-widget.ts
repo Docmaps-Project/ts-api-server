a79b0077af68dd66338c8f5b45e8b94de326acfc
Andrew Edstrom <andrewedstrom@gmail.com>
2023-09-25 15:00:06 -0600

    start project

---
+++ packages/widget/src/docmaps-widget.ts
@@ -0,0 +1,89 @@
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import { DocmapsFetchingController } from "./docmaps-fetching-controller";
+
+/**
+ * An example element.
+ *
+ * @fires count-changed - Indicates when the count changes
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('docmaps-widget')
+export class DocmapsWidget extends LitElement {
+  #fetchingController: DocmapsFetchingController = new DocmapsFetchingController(this);
+
+  static override styles = css`
+    :host {
+      display: block;
+      border: solid 1px gray;
+      padding: 16px;
+    }
+
+    details {
+      border: 1px solid #aaa;
+      border-radius: 4px;
+      padding: 0.5em 0.5em 0;
+    }
+
+    summary {
+      font-weight: bold;
+      margin: -0.5em -0.5em 0;
+      padding: 0.5em;
+    }
+
+    details[open] {
+      padding: 0.5em;
+    }
+
+    details[open] summary {
+      border-bottom: 1px solid #aaa;
+      margin-bottom: 0.5em;
+    }
+  `;
+
+  @property()
+  doi: string = 'N/A';
+
+  initialRender(){
+    return html`<p>Haven't even tried to fetch yet tbh</p>`
+  }
+
+  pendingRender(){
+    return html`<p>Loading...</p>`
+  }
+
+  renderAfterLoad(docmapArray: any[]) {
+    const docmap = docmapArray[0];
+    const result = JSON.stringify(docmap, null, 2);
+    return html`
+      <details open>
+        <summary>Raw Docmap</summary>
+        <pre>${result}</pre>
+      </details>
+    `
+  };
+
+  errorRender(err: unknown){
+    return html`<p>Couldn't fetch docmap: ${err}</p>`
+  }
+
+  override render() {
+    return html`
+      <h2>DOI: ${this.doi}</h2>
+      
+      ${this.#fetchingController.render({
+      initial: this.initialRender.bind(this),
+      pending: this.pendingRender.bind(this),
+      complete: this.renderAfterLoad.bind(this),
+      error: this.errorRender.bind(this),
+    })}
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'docmaps-widget': DocmapsWidget;
+  }
+}
