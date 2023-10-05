69dd2718d3c2c534c873b2d126cbe53faf5cf63f
Andrew Edstrom <andrewedstrom@gmail.com>
2023-10-05 14:27:20 -0600

    add more docmap examples

--- packages/docmaps-widget/src/docmaps-widget.ts
+++ packages/docmaps-widget/src/docmaps-widget.ts
@@ -13,6 +13,20 @@ import * as dagreD3 from "dagre-d3";
 import * as Dagre from "dagre";
 import * as d3 from "d3";
 
+const knownDocmapIds = [
+  "https://data-hub-api.elifesciences.org/enhanced-preprints/docmaps/v1/by-publisher/elife/get-by-doi?preprint_doi=10.1101%2F2022.11.08.515698",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-3171736/v1/rapid-reviews-covid-19.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-1020603/v1/prereview.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-1043992/v1/biophysics-colab.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-903562/v2/prereview.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-955726/v1/biophysics-colab.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-885194/v1/ncrc.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-871965/v1/ncrc.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-789831/v1/prereview.docmap.json",
+  "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-734203/v1/prereview.docmap.json",
+  // "https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-420780/v1/prereview.docmap.json",
+]
+
 @customElement("docmaps-widget")
 export class DocmapsWidget extends LitElement {
   @property()
@@ -122,12 +136,7 @@ export class DocmapsWidget extends LitElement {
       <br /><br />
       <p>Example docmaps:</p>
       <ul>
-        <li>
-          https://data-hub-api.elifesciences.org/enhanced-preprints/docmaps/v1/by-publisher/elife/get-by-doi?preprint_doi=10.1101%2F2022.11.08.515698
-        </li>
-        <li>
-          https://sciety.org/docmaps/v1/articles/10.21203/rs.3.rs-3171736/v1/rapid-reviews-covid-19.docmap.json
-        </li>
+        ${knownDocmapIds.map((id) => html`<li>${id}</li>`)}
       </ul>
       <input
         id="doi-input"
